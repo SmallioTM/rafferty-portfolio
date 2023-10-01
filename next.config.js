0da03dcdc26d0a8cf20073232973898e5c604238
SmallioTM <127532010+SmallioTM@users.noreply.github.com>
2023-10-01 23:53:22 +0300

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = {
+  // ... other Next.js configuration options ...
+
+  webpack: (config, { isServer }) => {
+    // Font configuration
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          publicPath: "/_next/static/fonts", // Adjust this path as needed
+          outputPath: "static/fonts", // Adjust this path as needed
+        },
+      },
+    });
+
+    return config;
+  },
+
+  // Define your font variables here
+  env: {
+    FONT_POLY: "PolySans Neutral, sans-serif",
+    FONT_FORT: "FortescueDisplay, serif",
+  },
+};
